7494284cb5d55c7ef6b30e152de267a9363571d6
reddyana <47602556+reddyana@users.noreply.github.com>
2021-10-22 16:04:06 -0400

    Add files via upload

--- app/src/main/java/com/example/test2/signInScreen.java
+++ app/src/main/java/com/example/test2/signInScreen.java
@@ -1,6 +1,8 @@
 package com.example.test2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,10 @@ public class signInScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_screen);
+    }
+    public void backToMain(View view) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
