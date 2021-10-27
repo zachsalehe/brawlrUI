55dbc5255ae4604f0635ffc088a602eb9b64d106
Thanos3 <anred432@gmail.com>
2021-10-27 11:15:54 -0400

    Added fragment screens for all tabs and set default to home screen, need to set up switching between screens

--- app/src/main/java/com/example/test2/HomeScreen.java
+++ app/src/main/java/com/example/test2/HomeScreen.java
@@ -2,20 +2,21 @@ package com.example.test2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class HomeScreen extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
+
     }
-
-
 }
