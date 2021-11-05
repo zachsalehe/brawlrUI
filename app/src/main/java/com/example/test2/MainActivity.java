b6e6e6beb9df1e3c827586fc69fba7ecf54e6642
Pierre Sarrailh <pierresarrailh@gmail.com>
2021-11-05 09:59:37 -0400

    added firebase

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.test2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
