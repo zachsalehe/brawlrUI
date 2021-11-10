0cef699733b2ddbedd51c183627da853fec4dc3b
Thanos3 <anred432@gmail.com>
2021-11-10 14:12:11 -0500

    Added swipe recognition to home screen

--- app/src/main/java/com/example/test2/HomeScreen.java
+++ app/src/main/java/com/example/test2/HomeScreen.java
@@ -1,9 +1,12 @@
 package com.example.test2;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -17,7 +20,20 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
-
+        RelativeLayout layout;
+        layout = findViewById(R.id.relativeLayout);
+        layout.setOnTouchListener(new OnSwipeTouchListener(HomeScreen.this) {
+            @Override
+            public void onSwipeLeft() {
+                super.onSwipeLeft();
+                Toast.makeText(HomeScreen.this, "Swipe Left gesture detected", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onSwipeRight() {
+                super.onSwipeRight();
+                Toast.makeText(HomeScreen.this, "Swipe Right gesture detected", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void homeScreen(View view){
